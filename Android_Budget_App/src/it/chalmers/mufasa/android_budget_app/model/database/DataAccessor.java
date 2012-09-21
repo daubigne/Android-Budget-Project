f6873b1a4e43bd1000d4aa05960b4b0045fa33c9
CaptainTec <alex.malmek@hotmail.com>
2012-09-21 12:39:00 +0200

    Added methods for transactions

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/model/database/DataAccessor.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/model/database/DataAccessor.java
@@ -1,15 +1,25 @@
 package it.chalmers.mufasa.android_budget_app.model.database;
 
+import java.util.Date;
+import java.util.List;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DataAccessor {
-
 	Context context;
 
 	public DataAccessor(Context context) {
 		this.context = context;
+	}
+
+	public enum SortBy {
+		NAME, DATE, CATEGORY
+	}
+
+	public enum SortByOrder {
+		DESC, ASC
 	}
 
 	public int getAccountBalance(int accountID) {
@@ -38,5 +48,71 @@ public class DataAccessor {
 					+ " WHERE id == " + accountID);
 		}
 
+	}
+
+	public void addTransaction(Transaction transaction) {
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+		db.execSQL("INSERT INTO transactions (account-id, name, date, value, category-id ) ("
+				+ transaction.getAccount().getId()
+				+ ", "
+				+ transaction.getName()
+				+ ", "
+				+ transaction.getDate()
+				+ ", "
+				+ transaction.getValue()
+				+ ", "
+				+ transaction.getCategory().getId() + ")");
+
+	}
+
+	public void removeTransaction(Transaction transaction) {
+
+	}
+
+	public List<Transaction> getTransactions(Account account, SortBy sortBy,
+			SortByOrder sortByOrder, int start, int stop) {
+
+		String sortByTemp;
+		String sortByOrderTemp;
+
+		switch (sortBy) {
+		case NAME:
+			sortByTemp = "name";
+			break;
+		case DATE:
+			sortByTemp = "date";
+			break;
+		case CATEGORY:
+			sortByTemp = "category";
+			break;
+		}
+		switch (sortByOrder) {
+		case ASC:
+			sortByOrderTemp = "asc";
+			break;
+		case DESC:
+			sortByOrderTemp = "desc";
+			break;
+		}
+
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+		String[] arr = { "name", "date", "category", "value" };
+
+		Cursor cursor = db.query("table", arr, "account-id == "
+				+ account.getAccountID, null, null, null, sortByTemp + " "
+				+ sortByOrderTemp);
+
+		if (cursor.moveToFirst()) {
+			cursor.move(start);
+			for (int i = start; i < stop; i++) {
+				return cursor.getString(1);
+
+				cursor.moveToNext();
+			}
+		} else {
+			return -1;
+		}
 	}
 }
