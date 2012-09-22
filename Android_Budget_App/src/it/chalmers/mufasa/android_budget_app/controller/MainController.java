54299c43c02c8023546796b4e11cb903ef03dfb2
Simon Nilsson <sim.nilsson@gmail.com>
2012-09-22 15:31:31 +0200

    Moved DataAccessor object to controller

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/MainController.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/MainController.java
@@ -1,15 +1,22 @@
 package it.chalmers.mufasa.android_budget_app.controller;
 
+import it.chalmers.mufasa.android_budget_app.controller.database.DataAccessor;
 import it.chalmers.mufasa.android_budget_app.model.MainModel;
+import android.content.Context;
 
 public class MainController {
 	MainModel model;
 	
-	public MainController(MainModel model) {
+	DataAccessor da;
+		
+	public MainController(Context context, MainModel model) {
 		this.model = model;
+		this.da = new DataAccessor(context);
+		this.setBalance(this.da.getAccountBalance(0));
 	}
 	
 	public void setBalance(int balance) {
+		this.da.setAccountBalance(balance, 0);
 		model.setBalance(balance);
 	}
 	
