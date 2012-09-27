386534067d06084ecd0d58f160e77a67338237ad
simphax <sim.nilsson@gmail.com>
2012-09-27 12:35:06 +0200

    Added list view

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/BudgetEditActivity.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/BudgetEditActivity.java
@@ -1,18 +1,41 @@
 package it.chalmers.mufasa.android_budget_app.activities;
 
 import it.chalmers.mufasa.android_budget_app.R;
-import it.chalmers.mufasa.android_budget_app.R.layout;
-import it.chalmers.mufasa.android_budget_app.R.menu;
-import android.os.Bundle;
+import it.chalmers.mufasa.android_budget_app.model.BudgetItem;
+import it.chalmers.mufasa.android_budget_app.model.Category;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BudgetEditActivity extends Activity {
 
+	private ListView listView;
+	private ArrayAdapter<BudgetItem> listAdapter;
+	private ArrayList<BudgetItem> budgetItemsList;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_edit);
+        
+        Category cat = new Category("Mat",1,null);
+        
+        budgetItemsList = new ArrayList<BudgetItem>();
+        budgetItemsList.add(new BudgetItem(1,cat,1000.0));
+        budgetItemsList.add(new BudgetItem(2,cat,2000.0));
+        budgetItemsList.add(new BudgetItem(3,cat,3000.0));
+        
+        listAdapter = new ArrayAdapter<BudgetItem>(this, R.layout.simplerow, budgetItemsList);
+        
+        
+        listView = (ListView) findViewById(R.id.budgetItemList);
+        listView.setAdapter(listAdapter); 
+        
     }
 
     @Override
