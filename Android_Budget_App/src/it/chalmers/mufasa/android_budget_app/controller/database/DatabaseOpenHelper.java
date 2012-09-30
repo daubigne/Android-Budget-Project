111183f04483ac023cec3452b1f04f3aaf5893df
simphax <sim.nilsson@gmail.com>
2012-09-30 20:03:33 +0200

    Added default database values

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/database/DatabaseOpenHelper.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/database/DatabaseOpenHelper.java
@@ -1,5 +1,6 @@
 package it.chalmers.mufasa.android_budget_app.controller.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,13 +19,63 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper{
 		db.execSQL("CREATE TABLE categories ( id INTEGER PRIMARY KEY, name TEXT, parentId INTEGER);");
 		db.execSQL("CREATE TABLE budgetitems ( id INTEGER PRIMARY KEY, categoryId INTEGER, value FLOAT NOT NULL);");
 
+		this.insertDefaultValues(db);
+	}
+	
+	private void insertDefaultValues(SQLiteDatabase db) {
+		//Assumes this is an empty database
+		
 		db.execSQL("INSERT INTO accounts (id,name,balance) VALUES (1,\"My Account\",0)");
 		db.execSQL("INSERT INTO settings (currentAccountId) VALUES (1)");
+		
+		//Values from average student between 18-30 years in sweden. Data from konsumentverket.se
+		db.execSQL("INSERT INTO categories (name) VALUES (\"Inkomster\")"); //id = 1
+		db.execSQL("INSERT INTO categories (name) VALUES (\"Utgifter\")"); //id = 2
+		
+		//Income
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Studiemedel\",1)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (3,8920)");
+		
+		//Expenses
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Mat\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (4,8920)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Hygien\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (5,370)");
 
-		db.execSQL("INSERT INTO categories (name) VALUES (\"Inkomster\")");
-		db.execSQL("INSERT INTO categories (name) VALUES (\"Utgifter\")");
-		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (1,9000)");
-		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (2,7000)");
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Kläder\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (6,600)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Fritid\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (7,630)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Fritid\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (7,630)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Mobiltelefon\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (8,180)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Förbrukningsvaror\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (9,100)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Medier\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (10,930)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Hemförsäkring\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (11,100)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Hyra\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (12,3200)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"El\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (13,250)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Kursliteratur\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (14,750)");
+		
+		db.execSQL("INSERT INTO categories (name,parentId) VALUES (\"Resor\",2)");
+		db.execSQL("INSERT INTO budgetitems (categoryId,value) VALUES (15,500)");
+		
 	}
 
 	@Override
