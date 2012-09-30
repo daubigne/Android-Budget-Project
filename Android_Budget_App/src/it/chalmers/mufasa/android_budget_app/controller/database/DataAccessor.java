0228f385ac6dcd2ecb7d0ba96c3403ca03d72c49
FannyM <alex.malmek@hotmail.com>
2012-09-30 16:06:05 +0200

    Updated dataAccessor

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/database/DataAccessor.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/database/DataAccessor.java
@@ -4,6 +4,7 @@ import it.chalmers.mufasa.android_budget_app.model.Account;
 import it.chalmers.mufasa.android_budget_app.model.Category;
 import it.chalmers.mufasa.android_budget_app.model.Transaction;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,7 +13,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-
+/**
+ * version 1
+ * 
+ * @author FannyM, Simphax
+ * 
+ *         A class that helps to translate between the database and the model.
+ * 
+ */
 public class DataAccessor {
 	private Context context;
 
@@ -29,79 +37,97 @@ public class DataAccessor {
 	}
 
 	public List<Account> getAccounts() {
-		
+
 		List<Account> accountList = new ArrayList<Account>();
-		
-		SQLiteDatabase db = new DatabaseOpenHelper(context).getWritableDatabase();
-		String[] arr = { "id", "name", "balance"};
-		Cursor cursor = db.query("accounts", arr, null, null,
-				null, null, null);
-		
+
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+		String[] arr = { "id", "name", "balance" };
+		Cursor cursor = db.query("accounts", arr, null, null, null, null, null);
+
 		if (cursor.moveToFirst()) {
-			accountList.add(new Account(cursor.getInt(0),cursor.getString(1),cursor.getDouble(2)));
-			
-			while(cursor.moveToNext()) {
-				accountList.add(new Account(cursor.getInt(0),cursor.getString(1),cursor.getDouble(2)));
+			accountList.add(new Account(cursor.getInt(0), cursor.getString(1),
+					cursor.getDouble(2)));
+
+			while (cursor.moveToNext()) {
+				accountList.add(new Account(cursor.getInt(0), cursor
+						.getString(1), cursor.getDouble(2)));
 			}
 		}
-		
+
 		return accountList;
-		
+
 	}
-	
+
 	public Account getAccount(int accountID) {
-		SQLiteDatabase db = new DatabaseOpenHelper(context).getWritableDatabase();
-		String[] arr = { "id", "name" , "balance" };
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+		String[] arr = { "id", "name", "balance" };
 		Cursor cursor = db.query("accounts", arr, "id == " + accountID, null,
 				null, null, null);
 
 		if (cursor.moveToFirst()) {
-			return new Account(cursor.getInt(0),cursor.getString(1),cursor.getDouble(2));
+			return new Account(cursor.getInt(0), cursor.getString(1),
+					cursor.getDouble(2));
 		} else {
-			throw new IllegalArgumentException("Account ID "+accountID+" does not exist");
+			throw new IllegalArgumentException("Account ID " + accountID
+					+ " does not exist");
 		}
 	}
 
 	public void addAccount(String name, double balance) {
-		SQLiteDatabase db = new DatabaseOpenHelper(context).getWritableDatabase();
-		
-		db.execSQL("INSERT INTO accounts (name, balance) VALUES (\""+name+"\","+balance+")");
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+
+		db.execSQL("INSERT INTO accounts (name, balance) VALUES (\"" + name
+				+ "\"," + balance + ")");
 	}
-	
+
 	public void setAccountBalance(Account account, double balance) {
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
 				.getWritableDatabase();
-		
+
 		double currentBalance = account.getBalance();
-		    if(currentBalance != balance) {
+		if (currentBalance != balance) {
 			db.execSQL("UPDATE accounts SET balance=" + balance
-				+ " WHERE id == " + account.getId());
-		    }
+					+ " WHERE id == " + account.getId());
+		}
 	}
 
-	public void addTransaction(Double amount, Date date, String name, Category category, Account account) {
+	public void addTransaction(Double amount, Date date, String name,
+			Category category, Account account) {
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
 				.getWritableDatabase();
+
+		// Creates a new date format and uses it to convert a Date into a String
+		// in order to save it into the database
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		String dateString = sdf.format(date);
+
 		db.execSQL("INSERT INTO transactions (accountId, name, date, value, categoryId ) VALUES ( "
 				+ account.getId()
 				+ ", "
-				+ "\"" + name + "\""
+				+ "\""
+				+ name
+				+ "\""
 				+ ", "
-				+ "\"" + date.getYear() + "-" + date.getMonth() + "-" + date.getDay() + "\""
+				+ "\""
+				+ dateString
+				+ "\""
 				+ ", "
 				+ amount
 				+ ", "
-				+ category.getId() + ")");
-		
-		this.setAccountBalance(account, account.getBalance() + amount);
-		
+				+ category.getId()
+				+ ")");
+
 	}
 
 	public void removeTransaction(Transaction transaction) {
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
 				.getWritableDatabase();
 		db.execSQL("DELETE FROM transactions WHERE id ==" + transaction.getId());
-		this.setAccountBalance(transaction.getAccount(), transaction.getAccount().getBalance() - transaction.getAmount());
+		this.setAccountBalance(transaction.getAccount(), transaction
+				.getAccount().getBalance() - transaction.getAmount());
 	}
 
 	public List<Transaction> getTransactions(Account account, SortBy sortBy,
@@ -111,8 +137,9 @@ public class DataAccessor {
 				.getWritableDatabase();
 		List<Transaction> transactions = new ArrayList<Transaction>();
 		String sortByTemp = "date";
-		String sortByOrderTemp = "desc";			
+		String sortByOrderTemp = "desc";
 
+		// A switch method to decide what identifier to sort transactions by
 		switch (sortBy) {
 		case NAME:
 			sortByTemp = "name";
@@ -124,6 +151,8 @@ public class DataAccessor {
 			sortByTemp = "category";
 			break;
 		}
+
+		// A switch method to decide how to order the transactions
 		switch (sortByOrder) {
 		case ASC:
 			sortByOrderTemp = "ASC";
@@ -135,91 +164,101 @@ public class DataAccessor {
 
 		String[] arr = { "name", "date", "id", "value" };
 
-		Cursor cursor = db.query("transactions", arr, "accountId == "
-				+ account.getId(), null, null, null, sortByTemp + " "
-				+ sortByOrderTemp);
+		Cursor cursor = db.query("transactions", arr,
+				"accountId == " + account.getId(), null, null, null, sortByTemp
+						+ " " + sortByOrderTemp);
 
-		if(cursor.moveToPosition(start)) {
-			for (int i = start; i < Math.min(start+count,cursor.getCount()); i++) {
+		if (cursor.moveToPosition(start)) {
+			for (int i = start; i < Math.min(start + count, cursor.getCount()); i++) {
 				String dateString = cursor.getString(1);
 				String[] list = dateString.split("-");
-				//Date date = new Date(Integer.parseInt(list[0]), Integer.parseInt(list[1]), Integer.parseInt(list[2]));
+				// Date date = new Date(Integer.parseInt(list[0]),
+				// Integer.parseInt(list[1]), Integer.parseInt(list[2]));
 				Date date = new Date(10000);
-				
-				Category category = new Category("untitled category",1,null);
-				Transaction transaction = new Transaction((cursor.getInt(3)), date, cursor.getString(2), category, account);
+
+				Category category = new Category("untitled category", 1, null);
+
+				// Creates a new transaction with the selected data from the
+				// database and adds the transaction to the list
+				Transaction transaction = new Transaction((cursor.getInt(3)),
+						date, cursor.getString(2), category, account);
 				transactions.add(transaction);
-				
+
 				cursor.moveToNext();
 			}
 		}
-		
+
 		return transactions;
 	}
-	
+
 	public List<Category> getCategories() {
-		
+
 		List<Category> list = new ArrayList<Category>();
-		
+
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
-		.getWritableDatabase();
+				.getWritableDatabase();
 		String[] arr = { "name", "id", "parentId" };
-		
-		Cursor cursor = db.query("categories", arr, null, null, null, null, null);
-		
+
+		Cursor cursor = db.query("categories", arr, null, null, null, null,
+				null);
+
 		if (cursor.moveToFirst()) {
-			Category category = new Category(cursor.getString(0), cursor.getInt(1), this.getCategory(cursor.getInt(2)));
+			Category category = new Category(cursor.getString(0),
+					cursor.getInt(1), this.getCategory(cursor.getInt(2)));
 			list.add(category);
-			
-			while(cursor.moveToNext()) {
-				category = new Category(cursor.getString(0), cursor.getInt(1), this.getCategory(cursor.getInt(2)));
+
+			while (cursor.moveToNext()) {
+				category = new Category(cursor.getString(0), cursor.getInt(1),
+						this.getCategory(cursor.getInt(2)));
 				list.add(category);
 			}
 		}
-		
+
 		return list;
 	}
-	
+
 	public Category getCategory(int id) {
-		SQLiteDatabase db = new DatabaseOpenHelper(context).getWritableDatabase();
-		
-		String[] arr = { "name", "id" , "parentId" };
-		
-		Cursor cursor = db.query("categories", arr, "id == " + id, null, null, null, null);
-		
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+
+		String[] arr = { "name", "id", "parentId" };
+
+		Cursor cursor = db.query("categories", arr, "id == " + id, null, null,
+				null, null);
+
 		if (cursor.moveToFirst()) {
-			return new Category(cursor.getString(0), cursor.getInt(1), this.getCategory(cursor.getInt(2)));
+			return new Category(cursor.getString(0), cursor.getInt(1),
+					this.getCategory(cursor.getInt(2)));
 		}
-		
+
 		return null;
 	}
-	
+
 	public void addCategory(String name, Category parent) {
-		SQLiteDatabase db = new DatabaseOpenHelper(context).getWritableDatabase();
-		
+		SQLiteDatabase db = new DatabaseOpenHelper(context)
+				.getWritableDatabase();
+
 		String parentId = "null";
-		if(parent != null) {
+		if (parent != null) {
 			parentId = String.valueOf(parent.getId());
 		}
-		
-		db.execSQL("INSERT INTO categories (name, parentId) VALUES ( "
-		+ "\"" + name + "\""
-		+ ", "
-		+ parentId
-		+ ")");
+
+		db.execSQL("INSERT INTO categories (name, parentId) VALUES ( " + "\""
+				+ name + "\"" + ", " + parentId + ")");
 
 	}
-	
-	public void removeCategory(Category category){
+
+	public void removeCategory(Category category) {
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
-		.getWritableDatabase();
-db.execSQL("DELETE FROM categories WHERE id ==" + category.getId());
+				.getWritableDatabase();
+		db.execSQL("DELETE FROM categories WHERE id ==" + category.getId());
 	}
-	
-	public void editCategory(Category category, String newName){
+
+	public void editCategory(Category category, String newName) {
 		SQLiteDatabase db = new DatabaseOpenHelper(context)
-		.getWritableDatabase();
-db.execSQL("UPDATE categories SET name = \"" + newName + "\" WHERE id == " + category.getId());
+				.getWritableDatabase();
+		db.execSQL("UPDATE categories SET name = \"" + newName
+				+ "\" WHERE id == " + category.getId());
 	}
-	
+
 }
