a33a69371957eb9426d4491748ef0bac450ab04c
FannyM <alex.malmek@hotmail.com>
2012-10-01 12:42:21 +0200

    finished merge

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/MainController.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/MainController.java
@@ -14,20 +14,9 @@ public class MainController {
 	public MainController(Context context, MainModel model) {
 		this.model = model;
 		this.dataAccessor = new DataAccessor(context);
-<<<<<<< HEAD
-		try {
-
-			model.setBalance(this.dataAccessor.getAccount(1).getBalance());
-		} catch (IllegalArgumentException e) { // Account 1 does not exist...
-			this.dataAccessor.addAccount("My Account",0);
-			model.setBalance(dataAccessor.getAccount(1).getBalance());
-
-		}
-=======
 		
 		model.setBalance(this.dataAccessor.getAccount(dataAccessor.getSettings().getCurrentAccountId()).getBalance());
-		
->>>>>>> refs/heads/dev
+
 	}
 
 	public void setBalance(double balance) {
