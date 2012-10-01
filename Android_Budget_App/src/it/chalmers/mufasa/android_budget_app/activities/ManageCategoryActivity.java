e506ea5becef763f60cfdf549f73f6fc8304b4a4
FannyM <alex.malmek@hotmail.com>
2012-10-01 12:09:00 +0200

    More skeleton methods

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/ManageCategoryActivity.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/ManageCategoryActivity.java
@@ -1,21 +1,44 @@
 package it.chalmers.mufasa.android_budget_app.activities;
 
+import it.chalmers.mufasa.android_budget_app.R;
+import it.chalmers.mufasa.android_budget_app.controller.MainController;
 import it.chalmers.mufasa.android_budget_app.controller.ManageCategoryController;
 import it.chalmers.mufasa.android_budget_app.model.MainModel;
-import it.chalmers.mufasa.android_budget_app.model.ManageCategoryModel;
-import it.chalmers.mufasa.android_budget_app.model.ModelListener;
+import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class ManageCategoryActivity extends Activity implements ModelListener{
+public class ManageCategoryActivity extends Activity {
 
-	ManageCategoryController controller;
-	ManageCategoryModel model;
+	private ManageCategoryController controller;
+	private MainModel model;
 	
-	
-	
-	public void onChange(MainModel model) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_category);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_manage_category, menu);
+        return true;
+    }
+    
+    public void addCategory(View view) {
+    	controller.addCategory(name, parent);
+    }
+    
+    public void removeCategory(View view) {
+    	controller.removeCategory(category);
+    }
+    
+    public void editCategory(View view) {
+    	controller.editCategory(category, newName);
+    }
+    
+    
 }
--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/ManageCategoryController.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/ManageCategoryController.java
@@ -1,5 +1,34 @@
 package it.chalmers.mufasa.android_budget_app.controller;
 
-public class ManageCategoryController {
+import android.content.Context;
+import android.view.View;
+import it.chalmers.mufasa.android_budget_app.controller.database.DataAccessor;
+import it.chalmers.mufasa.android_budget_app.model.Category;
+import it.chalmers.mufasa.android_budget_app.model.MainModel;
+import it.chalmers.mufasa.android_budget_app.model.ManageCategoryModel;
 
+public class ManageCategoryController {
+	
+	private ManageCategoryModel model;
+	private DataAccessor dataAccessor;
+	
+	public ManageCategoryController(Context context, ManageCategoryModel model) {
+		this.model = model;
+		this.dataAccessor = new DataAccessor(context);
+	}
+	
+	
+	public void addCategory(String name, Category parent) {
+    	dataAccessor.addCategory(name, parent);
+    }
+	
+	public void removeCategory(Category category) {
+    	dataAccessor.removeCategory(category);
+    }
+	
+	public void editCategory(Category category, String newName) {
+    	dataAccessor.editCategory(category, newName);
+    }
+	
+	
 }
