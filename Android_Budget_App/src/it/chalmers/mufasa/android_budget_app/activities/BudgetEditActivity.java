cae84dda0f17bb4de40a53b3f20aad8d189eded7
simphax <sim.nilsson@gmail.com>
2012-10-05 14:51:32 +0200

    Added class comment. Removed unused code.

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/BudgetEditActivity.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/BudgetEditActivity.java
@@ -15,24 +15,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
+/**
+ * Activity for managing the users current budget. Includes adding, editing and removing budgetitem rows.
+ * 
+ * @author Simon
+ *
+ */
 public class BudgetEditActivity extends Activity implements PropertyChangeListener {
 
 	private BudgetEditController controller;
 	private BudgetEditModel model;
-	
-	private ListView listView;
-	private EditText categoryIdEditText;
-	private EditText valueEditText;
-	
-	private List<BudgetItem> budgetItemList;
-	private ArrayAdapter<BudgetItem> listAdapter;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,16 +40,6 @@ public class BudgetEditActivity extends Activity implements PropertyChangeListen
         this.controller = new BudgetEditController(this.getApplicationContext(), model);
         
         model.addPropertyChangeListener(this);
-        
-        //Category cat = new Category("Mat",1,null);
-        
-        //categoryIdEditText = (EditText) findViewById(R.id.categoryId);
-        //valueEditText = (EditText) findViewById(R.id.value);
-        
-        //listAdapter = new ArrayAdapter<BudgetItem>(this, R.layout.simplerow, model.getBudgetItems());
-        
-        //listView = (ListView) findViewById(R.id.budgetItemList);
-        //listView.setAdapter(listAdapter);
         
         this.populateBudgetListView(model.getBudgetItems());
     }
@@ -106,23 +93,6 @@ public class BudgetEditActivity extends Activity implements PropertyChangeListen
     public void addBudgetItemRow(View view) {
     	controller.addBudgetItem(model.getCurrentMainCategory(), 0.0);
     }
-    /*
-    public void addBudgetItem(View view) {
-    	//int categoryId = Integer.parseInt(this.categoryIdEditText.getText().toString());
-    	double value = Double.parseDouble(this.valueEditText.getText().toString());
-    	Category cat;
-    	
-    	if(value > 0) {
-    		cat = new Category("Inkomster",1,null); //TODO get category from ChooseCategoryActivity
-    	} else if(value < 0) {
-    		cat = new Category("Utgifter",2,null); 
-    	} else {
-    		return;//TODO Alert user value can not be zero?
-    	}
-    	
-    	controller.addBudgetItem(cat, value);
-    }
-    */
     
     public void enterEditMode(View view) {
     	controller.setEditMode(true);
