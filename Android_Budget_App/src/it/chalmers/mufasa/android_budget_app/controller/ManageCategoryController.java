eac279f9c421e1232fe6075d54dc6f7bb5bfe32d
FannyM <alex.malmek@hotmail.com>
2012-10-07 21:16:53 +0200

    Removed database from controller

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/ManageCategoryController.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/controller/ManageCategoryController.java
@@ -8,45 +8,31 @@ import android.content.Context;
 /**
  * version 1
  * 
- * @author FannyM A class which updates the model via the database.
+ * @author FannyM A class which updates the model.
  */
 
 public class ManageCategoryController {
 
 	private ManageCategoryModel model;
-	private DataAccessor dataAccessor;
 
-	public ManageCategoryController(Context context, ManageCategoryModel model) {
+	public ManageCategoryController(ManageCategoryModel model) {
 		this.model = model;
-		this.dataAccessor = new DataAccessor(context);
-		model.setCurrentParentCategory(dataAccessor.getCategory(2));
-		model.setEditMode(false);
-		model.setList(dataAccessor.getCategories(model
-				.getCurrentParentCategory()));
 	}
 
 	public void setCurrentParentCategory(int id) {
-		model.setCurrentParentCategory(dataAccessor.getCategory(id));
-		model.setList(dataAccessor.getCategories(model
-				.getCurrentParentCategory()));
+		model.setCurrentParentCategory(id);
 	}
 
 	public void addCategory(String name, Category parent) {
-		dataAccessor.addCategory(name, parent);
-		model.setList(dataAccessor.getCategories(model
-				.getCurrentParentCategory()));
+		model.addCategory(name, parent);
 	}
 
 	public void removeCategory(Category category) {
-		dataAccessor.removeCategory(category);
-		model.setList(dataAccessor.getCategories(model
-				.getCurrentParentCategory()));
+		model.removeCategory(category);
 	}
 
 	public void editCategory(Category category, String newName) {
-		dataAccessor.editCategory(category, newName);
-		model.setList(dataAccessor.getCategories(model
-				.getCurrentParentCategory()));
+		model.editCategory(category, newName);
 	}
 
 	public void setEditMode(boolean b) {
