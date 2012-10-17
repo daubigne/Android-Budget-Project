d5b11d54c17230b857affa1ecc533a53d95db424
Slurpo <lurpen42@gmail.com>
2012-10-17 16:12:55 +0200

    Removed unused classes

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/activities/DateDialogFragmentListener.java
+++
@@ -1,7 +0,0 @@
-package it.chalmers.mufasa.android_budget_app.activities;
-
-import java.util.Calendar;
-
-public interface DateDialogFragmentListener{
-    public void dateDialogFragmentDateSet(Calendar date);
-}
