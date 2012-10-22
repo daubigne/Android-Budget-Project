b9f384665038d0f29dbd8fbca8069def60e789eb
FannyM <alex.malmek@hotmail.com>
2012-10-22 17:03:10 +0200

    budgetEditModel -  removed unused code and made variables private

--- Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/model/BudgetEditModel.java
+++ Android_Budget_App/src/it/chalmers/mufasa/android_budget_app/model/BudgetEditModel.java
@@ -1,65 +1,62 @@
- /*
-  * Copyright © 2012 Mufasa developer unit
-  *
-  * This file is part of Mufasa Budget.
-  *
-  *	Mufasa Budget is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * Mufasa Budget is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with Mufasa Budget.  If not, see <http://www.gnu.org/licenses/>.
-  */
+/*
+ * Copyright © 2012 Mufasa developer unit
+ *
+ * This file is part of Mufasa Budget.
+ *
+ *	Mufasa Budget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mufasa Budget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mufasa Budget.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.chalmers.mufasa.android_budget_app.model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.content.Context;
 
 /**
- * Model to represent the state of BudgetEditFragment.
- * Calls appropriate methods in Account to store BudgetItems in database.
+ * Model to represent the state of BudgetEditFragment. Calls appropriate methods
+ * in Account to store BudgetItems in database.
  * 
  * @author simphax <sim.nilsson@gmail.com>
- *
+ * 
  */
 public class BudgetEditModel {
 
-	Account account;
-	List<BudgetItem> budgetItems;
-	
-	PropertyChangeSupport pcs;
-	
-	Category currentMainCategory;
-	
-	boolean editMode = false;
-	
+	private Account account;
+	private PropertyChangeSupport pcs;
+	private Category currentMainCategory;
+	private boolean editMode = false;
+
 	public BudgetEditModel(Context context) {
 		this.pcs = new PropertyChangeSupport(this);
 		this.account = Account.getInstance(context);
-		this.currentMainCategory = account.getCategory(1);		
-		
+		this.currentMainCategory = account.getCategory(1);
+
 	}
-	
+
 	public List<BudgetItem> getBudgetItems() {
-		System.out.println("Getting budget items with category"+this.getCurrentMainCategory().getName());
+		System.out.println("Getting budget items with category"
+				+ this.getCurrentMainCategory().getName());
 		return account.getBudgetItems(this.getCurrentMainCategory());
 	}
-	
-	public void setCurrentMainCategory(int i) { //TODO get category from choosecategoryactivity
+
+	public void setCurrentMainCategory(int i) { // TODO get category from
+												// choosecategoryactivity
 		this.currentMainCategory = account.getCategory(i);
-		pcs.firePropertyChange("updated_current_category", null, null); 
+		pcs.firePropertyChange("updated_current_category", null, null);
 	}
-	
+
 	public Category getCurrentMainCategory() {
 		return this.currentMainCategory;
 	}
@@ -68,44 +65,44 @@ public class BudgetEditModel {
 		account.addBudgetItem(this.getCurrentMainCategory(), 0.0);
 		pcs.firePropertyChange("new_budgetitem", null, null);
 	}
-	
+
 	public void removeBudgetItem(BudgetItem item) {
 		account.removeBudgetItem(item);
 		pcs.firePropertyChange("removed_budgetitem", null, null);
 	}
-	
+
 	public void replaceBudgetItems(List<BudgetItem> budgetItems) {
-		List<BudgetItem> removeList = account.getBudgetItems(this.getCurrentMainCategory());
-		
-		for(BudgetItem item : removeList) {
+		List<BudgetItem> removeList = account.getBudgetItems(this
+				.getCurrentMainCategory());
+
+		for (BudgetItem item : removeList) {
 			account.removeBudgetItem(item);
 		}
-		
-		for(BudgetItem item : budgetItems) {
+
+		for (BudgetItem item : budgetItems) {
 			account.addBudgetItem(item.getCategory(), item.getValue());
 		}
-		
-		pcs.firePropertyChange("replaced_budgetitem_list", null, null); 
+
+		pcs.firePropertyChange("replaced_budgetitem_list", null, null);
 	}
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		pcs.addPropertyChangeListener(listener);
 	}
-	
+
 	public void removePropertyChangeListener(PropertyChangeListener listener) {
 		pcs.removePropertyChangeListener(listener);
 	}
-	
+
 	public void setEditMode(boolean set) {
-		if(this.editMode != set) {
+		if (this.editMode != set) {
 			this.editMode = set;
 			pcs.firePropertyChange("changed_editmode", null, null);
 		}
 	}
-	
+
 	public boolean isEditMode() {
 		return this.editMode;
 	}
 
-	
 }
